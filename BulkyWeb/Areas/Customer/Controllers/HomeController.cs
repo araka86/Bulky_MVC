d9b5aefbcd647fc73f1246bb3bd9c8861fc85de4
Ruslan Pushniak <araka86@gmail.com>
2025-01-14 21:19:42 +0200

    main page and details

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,81 +1,47 @@
 using System.Diagnostics;
+using Bulcky.DataAcess.Repository.IRepository;
 using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Areas.Customer.Controllers
 {
 
-    class MyClass
-    {
-
-
-        public int Value { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            if (obj is MyClass other)
-            {
-                return this.Value == other.Value; // сравнение по значению
-            }
-            return false;
-        }
-
-        public override int GetHashCode() => Value.GetHashCode();
-    }
-
-
     [Area("Customer")]
     public class HomeController : Controller
     {
 
 
-
-        public struct Point
-        {
-            public int X { get; set; }
-           
-
-            public Point()
-            {
-                X = 10;
-           
-            }
-        }
-
-
-
-
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
 
-           
-           
-
-
-            MyClass obj1 = new MyClass { Value = 5 };
-            MyClass obj2 = new MyClass { Value = 5 };
-
-         
-            var qqq = (obj1 == obj2); // False Ч сравнение ссылок
-            var qqq1 = obj1.Equals(obj2); // True Ч сравнение по значению
-
-          Point point = new Point();
-            var ppp = point.X;
-            point.X = 20;
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
         }
+
+
+        public IActionResult Details(int productId)
+        {
+
+            Product product = _unitOfWork.Product.Get(x=>x.Id == productId, includeProperties: "Category");
+            return View(product);
+        }
+
+
 
         public IActionResult Privacy()
         {
             return View();
         }
+      
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
