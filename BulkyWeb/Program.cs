ffdd27a3cca412557e10d5a469cef62541b2f2d7
Ruslan Pushniak <araka86@gmail.com>
2025-01-22 01:11:46 +0200

    register and login finish

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -2,6 +2,9 @@ using Bulcky.DataAcess.Repository;
 using Bulcky.DataAcess.Repository.IRepository;
 using Bulky.DataAcess.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Bulky.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,8 +15,34 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=> 
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultStringConnections")));
 
+
+
+
+// дл€ подтверждени€ почты
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+// без подтверждени€ почты + роли
+builder.Services.AddIdentity<IdentityUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+
+
+//дл€ перенаправлени€ зарегестрированих пользователей на страницу Access denied и дл€ незарегестрированых на сроницу Login
+builder.Services.ConfigureApplicationCookie(option =>
+{
+    option.LoginPath = $"/Identity/Account/Login";
+    option.LogoutPath = $"/Identity/Account/Logout";
+    option.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
+
+
+
+
+//поддержка mvvm
+builder.Services.AddRazorPages();
+
 //DI
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
@@ -30,7 +59,12 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+
+app.UseAuthentication();
 app.UseAuthorization();
+
+//поддержка mvvm
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
